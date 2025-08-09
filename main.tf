1371360248b87ba92affd33235f650ffe19fcb6e
Ashokkumar-21 <116096679+Ashokkumar-21@users.noreply.github.com>
2025-08-09 19:33:20 +0530

    Main.tf Udpate 7:33

--- main.tf
+++ main.tf
@@ -31,6 +31,7 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
   cluster_name    = var.cluster_name
   cluster_version = "1.29"
   subnet_ids      = module.vpc.private_subnets
@@ -53,4 +54,5 @@ module "eks" {
   }
 
 }
+
 
