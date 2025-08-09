004f7f539ede8aabc033973f87220188ab48d313
Ashokkumar-21 <116096679+Ashokkumar-21@users.noreply.github.com>
2025-08-09 20:01:20 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,13 +1,14 @@
 output "cluster_name" {
+  description = "EKS cluster name"
   value = module.eks.cluster_name
 }
 
 output "cluster_endpoint" {
+  description = "EKS cluster endpoint"
   value = module.eks.cluster_endpoint
 }
 
 output "kubeconfig" {
   description = "Kubeconfig for accessing the EKS cluster"
-  value       = module.eks.kubeconfig
-  sensitive   = true
+  value       = module.eks.cluster_certificate_authority_data
 }
