8bfb22ffacefae09dd643eb4b9fb5ce31e6a9d8d
Ashokkumar-21 <116096679+Ashokkumar-21@users.noreply.github.com>
2025-08-12 23:38:01 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -53,7 +53,7 @@ resource "aws_eks_node_group" "node_group" {
     min_size     = 1
   }
 
-  instance_types = ["t2.micro"]
+  instance_types = ["t3.medium"]
 
   remote_access {
     ec2_ssh_key               = var.key_name
